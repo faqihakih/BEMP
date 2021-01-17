1e434200c04f372076b5cf7f2eba627f41bc8b4d
faqihakih <faqihakih12@gmail.com>
2021-01-17 19:56:29 +0700

    add index router

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,12 @@ const mainRouter = express.Router();
 
 const authRouter = require('./authRouter');
 const tourismRouter = require('./tourismRouter');
+const welcomeRouter = require('./welcomeRouter');
+
 
 const {checkLogin} = require('../helpers/checkUser')
 
+mainRouter.use('/', welcomeRouter);
 mainRouter.use('/auth', authRouter);
 mainRouter.use('/tourism', checkLogin, tourismRouter);
 
